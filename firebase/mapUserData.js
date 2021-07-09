ac1e5412f52e114ad9f518ac4006c8f8ead91106
Francoveliz <francoleandroveliz@gmail.com>
2021-07-09 11:28:00 -0300

    auth setup

---
+++ firebase/mapUserData.js
@@ -0,0 +1,12 @@
+export const mapUserData = user => {
+	// const { uid, email, xa, displayName, photoUrl } = user;
+	// return {
+	// 	id: uid,
+	// 	email,
+	// 	token: xa,
+	// 	name: displayName,
+	// 	profilePic: photoUrl,
+	// };
+
+	return user;
+};
