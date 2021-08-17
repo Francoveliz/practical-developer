3303afa018dfb5e2725531522589c228da14cda6
Francoveliz <francoleandroveliz@gmail.com>
2021-08-17 20:18:51 -0300

    fixed production error

--- pages/challenges/index.js
+++ pages/challenges/index.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "../../components/Card";
 import { SimpleGrid } from "@chakra-ui/layout";
 
-const Challenges = ({ challenges = [] }) => {
+const Challenges = () => {
+	const [challenges, setChallenges] = useState([]);
+
+	const fetchChallenges = async () => {
+		try {
+			const res = await axios.get(`http://localhost:3000/api/challenges`);
+			const data = await res.data;
+			setChallenges(() => data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchChallenges();
+	}, []);
+
 	return (
 		<>
 			<SimpleGrid columns={4} spacing={6}>
@@ -17,10 +33,10 @@ const Challenges = ({ challenges = [] }) => {
 
 export default Challenges;
 
-export async function getStaticProps(context) {
-	const res = await axios.get("http://localhost:3000/api/challenges");
-	const challenges = res.data;
-	return {
-		props: { challenges }, // will be passed to the page component as props
-	};
-}
+// export async function getStaticProps(context) {
+// 	const res = await axios.get("http://localhost:3000/api/challenges");
+// 	const challenges = res.data;
+// 	return {
+// 		props: { challenges }, // will be passed to the page component as props
+// 	};
+// }
